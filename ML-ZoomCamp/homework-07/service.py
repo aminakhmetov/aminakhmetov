695aee586155d0dbf5f5ce11fc5eb5a0b4c12a65
Artur Minakhmetov <artur.minakhmetov@telecom-paristech.fr>
2022-10-23 21:59:54 +0200

    Homework of 7th week

---
+++ ML-ZoomCamp/homework-07/service.py
@@ -0,0 +1,43 @@
+import bentoml
+from bentoml.io import JSON
+from pydantic import BaseModel
+
+class CreditApplication(BaseModel):
+    seniority: int
+    home: str
+    time: int
+    age: int
+    marital: str
+    records: str
+    job: str
+    expenses: int
+    income: float
+    assets: float
+    debt: float
+    amount: int
+    price: int
+
+
+model_ref = bentoml.xgboost.get("credit_risk_model:latest")
+dv = model_ref.custom_objects['dictVectorizer']
+
+model_runner = model_ref.to_runner()
+
+svc = bentoml.Service("credit_risk_classifier", runners = [model_runner])
+
+@svc.api(input=JSON(pydantic_model=CreditApplication),output=JSON())
+
+def classify(credit_application):
+    application_data = credit_application.dict()
+    vector = dv.transform(application_data)
+    prediction = model_runner.predict.run(vector)
+    print(prediction)
+    
+    result = prediction[0]
+    
+    if result > 0.5:
+        return {"status":"DECLINED"}
+    elif result > 0.23:
+        return {"status":"MAYBE"}
+    else:
+        return {"status":"APPROVED"}
---
+++ ML-ZoomCamp/homework-07/service_sklearn.py
@@ -0,0 +1,13 @@
+import bentoml
+from bentoml.io import NumpyNdarray
+
+model_ref = bentoml.sklearn.get("mlzoomcamp_homework:latest")
+model_runner = model_ref.to_runner()
+
+svc = bentoml.Service("mlzoomcamp_homework", runners = [model_runner])
+
+@svc.api(input=NumpyNdarray(), output=NumpyNdarray())
+def predict(input):
+    prediction = model_runner.predict.run(input)
+    print(prediction)
+    return prediction
